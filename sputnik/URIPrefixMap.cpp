c9059735c3f0ef06eb353230a1f57c0bd3bc39ea
Andris Pavēnis <andris.pavenis@fmi.fi>
2022-09-20 11:04:58 +0300

    URIPrefixMap: refactor and add service sequence number to ID of service (internal use only)

--- sputnik/URIPrefixMap.cpp
+++ sputnik/URIPrefixMap.cpp
@@ -7,30 +7,48 @@ namespace ba = boost::algorithm;
 URIPrefixMap::URIPrefixMap() = default;
 URIPrefixMap::~URIPrefixMap() = default;
 
+namespace
+{
+    std::string backend_service_id(const std::string& prefix,
+                                   const SmartMet::BackendServicePtr& backendService)
+    {
+        const auto backend_ptr = backendService->Backend();
+        const std::string id = backend_ptr->Name()
+            + ":" + std::to_string(backend_ptr->Port())
+            + ":" + std::to_string(backendService->SequenceNumber());
+        return id;
+    }
+}
+
 void URIPrefixMap::addPrefix(const std::string& prefix, const BackendServicePtr& backendService)
 {
-    const auto backend_ptr = backendService->Backend();
-    const std::string host_port = backend_ptr->Name() + ":" + std::to_string(backend_ptr->Port());
-#if defined(MYDEBUG)
-    std::cout << boost::posix_time::second_clock::local_time() << " Add prefix '" << prefix
-              << " for '" << host_port << std::endl;
-#endif
+    const std::string id = backend_service_id(prefix, backendService);
     std::unique_lock<std::mutex> lock(mutex);
-    prefixMap[prefix].insert(host_port);
+    if (prefixMap[prefix].insert(id).second) {
+#if defined(MYDEBUG)
+        std::cout << "URIPrefixMap::addPrefix: prefix=" << prefix << " id=" << id << std::endl;
+#endif
+    } else {
+        std::cout << "URIPrefixMap::addPrefix: prefix=" << prefix
+                  << " failed to remove id " << id << std::endl;
+    }
 }
 
 void URIPrefixMap::removeBackend(const std::string& prefix, const BackendServicePtr& backendService)
 {
-    const auto backend_ptr = backendService->Backend();
-    const std::string host_port = backend_ptr->Name() + ":" + std::to_string(backend_ptr->Port());
-#if defined(MYDEBUG)
-    std::cout << boost::posix_time::second_clock::local_time() << " Remove prefix '" << prefix
-              << " from '" << host_port << std::endl;
-#endif
+    const std::string id = backend_service_id(prefix, backendService);
     std::unique_lock<std::mutex> lock(mutex);
     decltype(prefixMap)::iterator curr = prefixMap.find(prefix);
     if (curr != prefixMap.end()) {
-        curr->second.erase(host_port);
+        if (curr->second.erase(id)) {
+#if defined(MYDEBUG)
+            std::cout << "URIPrefixMap::removeBackend: prefix=" << prefix
+                      << " id=" << id << " : removed" << std::endl;
+#endif
+        } else {
+            std::cout << "URIPrefixMap::removeBackend: prefix=" << prefix
+                      << " id=" << id << " : failed to remove (not found)" << std::endl;
+        }
     }
     if (curr->second.empty()) {
         prefixMap.erase(curr);
